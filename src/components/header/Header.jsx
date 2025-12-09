f6861292159e76eeb4738d05385eee85c08f764e
Ewerton Hecsley <ewerton.martinscomercial@gmail.com>
2025-12-09 15:30:28 -0300

    refactor: create Hamburguer menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,28 +1,45 @@
-import { Link } from 'react-router-dom';
-import s from './Header.module.scss';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import s from "./Header.module.scss";
 
 export const Header = () => {
-    return (
-        <>
-            <header className={s.header}>
-                <section className={s.headerBoxLeft}>
-                    <img src="src/assets/logo.png" alt="logo de coracao" />
-                    <Link to="/">
-                        <p>Médicos & Dentistas</p>
-                    </Link>
-                </section>
-                <section className={s.headerBoxRigth}>
-                    <nav>
-                        <Link to="/">
-                            <p>Home</p>
-                        </Link>
-                        <Link to="/voluntario">
-                            <button>Seja voluntário</button>
-                        </Link>
-                    </nav>
-                </section>
-            </header >
-            <hr/>
-        </>
-    )
-}
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggleMenu = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  return (
+    <>
+      <header className={s.header}>
+        <section className={s.headerBoxLeft}>
+          <img src="src/assets/logo.png" alt="logo de coracao" />
+          <Link to="/">
+            <p>Médicos & Dentistas</p>
+          </Link>
+        </section>
+
+        <button
+          className={`${s.hamburger} ${isOpen ? s.hamburgerOpen : ""}`}
+          onClick={handleToggleMenu}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+
+        <section className={s.headerBoxRigth}>
+          <nav className={isOpen ? s.navOpen : ""}>
+            <Link to="/">
+              <p>Home</p>
+            </Link>
+            <Link to="/voluntario">
+              <button>Seja voluntário</button>
+            </Link>
+          </nav>
+        </section>
+      </header>
+      <hr />
+    </>
+  );
+};
