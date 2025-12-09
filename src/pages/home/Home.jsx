d82c0d29befa73d063436e15c9dc2acfe9caa501
Ewerton Hecsley <ewerton.martinscomercial@gmail.com>
2025-12-09 17:16:32 -0300

    feat: add content main, first part

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,31 @@
+import { useNavigate } from 'react-router-dom'
+import s from './Home.module.scss'
+
 export const Home=()=>{
+    const navigate = useNavigate();
+
+    const handlerClick = () => {
+        navigate("/voluntario");
+    }
     return(
-        <main>
-            <h1>Home Page</h1>
+        <main className={s.main}>
+            <section className={s.mainContentOne}>
+                <section className={s.boxLeft}>
+                    <img src="src/assets/imagem.png" alt="imagem do nome da logo" />
+                    <h2>Saúde e cuidado sem barreiras</h2>
+                    <p>Um projeto dedicado a oferecer atendimento médico e odontológico gratuito para pessoas que mais precisam na nossa comunidade.</p>
+                    <div className={s.boxButton}>
+                        <button onClick={handlerClick} className={s.buttonVoluntario}>Seja Voluntário</button>
+                        <button className={s.buttonAjudar}>Como Ajudar</button>
+                    </div>
+                </section>
+                <section className={s.boxRigth}>
+                    <div className={s.boxImage}>
+                        
+                    </div>
+                </section>
+            </section>
+            <section className={s.mainContentTwo}></section>
         </main>
     )
 }
